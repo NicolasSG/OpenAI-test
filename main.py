4e4f2d5f5bef3ef62c531ae030d336e34e3f3a65
NicolasSG <neco.beatles@gmail.com>
2025-05-13 18:20:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
         temperature=0,  # este é o grau de aleatoriedade da saída do modelo
     )
     # retornar o conteúdo da resposta
-    return completion.choices[0].message["content"]
+    return completion.choices[0].message["content"] 
 
 if __name__ == '__main__':
 
